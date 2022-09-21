ae453cf1b660387c913ebde049b9ee7134cbe506
Stephanie Puertas <80504918+stephaniepuertas@users.noreply.github.com>
2022-09-21 14:57:59 -0700

    Add files via upload

---
+++ button_clicker/main.js
@@ -0,0 +1,10 @@
+// function for logout button
+function clicked (element){
+    console.log ("log out");
+    element.innerText = "log out";
+}
+// function for definition button
+function deletedef (element){
+    console.log (" ");
+    element.remove()
+}
